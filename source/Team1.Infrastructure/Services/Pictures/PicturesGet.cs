0eab003f7d5ba5a5dce4fad755978c35449ae9dd
Jeremy Smith <jsmith@mca.net>
2024-02-08 15:54:47 -0500

    updated logo on dark mode.

--- source/Team1.Infrastructure/Services/Pictures/PicturesGet.cs
+++ source/Team1.Infrastructure/Services/Pictures/PicturesGet.cs
@@ -1,161 +1,155 @@
 using Microsoft.EntityFrameworkCore;
 using Services.FileManager;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Team1.Infrastructure.Dtos;
 using Team1.Infrastructure.Dtos.Helpers;
 using Team1.Model.Enums;
 
-namespace Team1.Infrastructure.Services.Pictures
+namespace Team1.Infrastructure.Services.Pictures;
+
+public class PicturesGet : BaseService
 {
-    public class PicturesGet : BaseService
+  private IFileManager _fileManager;
+
+  public PicturesGet(IFileManager fileManager)
+  {
+    _fileManager = fileManager;
+  }
+
+  /// <summary>
+  /// Gets all Pictures that the logged in user has access to.
+  /// </summary>
+  /// <param name="activeOnly">active only items</param>
+  public Task<List<PictureDto>> GetPictures(bool activeOnly)
+  {
+    return (from x in db.PicturesByFilter(UserPermissionService, activeOnly)
+            select new PictureDto()
+            {
+              IsApproved = x.ApprovedDateTime.HasValue,
+              Description = x.Description,
+              PictureId = x.PictureId,
+              IsActive = !x.AuditFields.InactiveDateTime.HasValue,
+            }).ToListAsync();
+  }
+
+  /// <summary>
+  /// Gets all pictures for gallery
+  /// </summary>
+  /// <param name="activeOnly">active only items</param>
+  public Task<List<PictureDto>> GetPicturesForBrowsing()
+  {
+    return (from x in db.PicturesByFilter(UserPermissionService, true)
+            where x.ApprovedDateTime.HasValue
+            select new PictureDto()
+            {
+              IsApproved = x.ApprovedDateTime.HasValue,
+              Description = x.Description,
+              PictureId = x.PictureId,
+              IsActive = !x.AuditFields.InactiveDateTime.HasValue,
+            }).ToListAsync();
+  }
+
+  /// <summary>
+  /// Gets random pictures
+  /// </summary>
+  public Task<List<PictureDto>> GetRandomPictures()
+  {
+    return (from x in db.PicturesByFilter(UserPermissionService, true)
+            where x.ApprovedDateTime.HasValue
+            select new PictureDto()
+            {
+              IsApproved = x.ApprovedDateTime.HasValue,
+              Description = x.Description,
+              PictureId = x.PictureId,
+              IsActive = !x.AuditFields.InactiveDateTime.HasValue,
+            }).OrderBy(o => Guid.NewGuid()).Take(5).ToListAsync();
+  }
+
+  /// <summary>
+  /// Gets my Pictures
+  /// </summary>
+  public Task<List<PictureDto>> GetMyPictures()
+  {
+    return (from x in db.PicturesByFilter(UserPermissionService, false)
+            where x.OwnerUserId == UserPermissionService.UserClaimModel.UserId
+            select new PictureDto()
+            {
+              IsApproved = x.ApprovedDateTime.HasValue,
+              Description = x.Description,
+              PictureId = x.PictureId,
+              IsActive = !x.AuditFields.InactiveDateTime.HasValue,
+            }).ToListAsync();
+  }
+
+  /// <summary>
+  /// Gets gallery types the user has access to
+  /// </summary>
+  public List<SelectOptionDto<byte>> GetGalleryTypes()
+  {
+    return GalleryTypeEnum.AdminMode.GetList().Where(w =>
     {
-        private IFileManager _fileManager;
+      switch (w)
+      {
+        case GalleryTypeEnum.Public:
+          return true;
+        case GalleryTypeEnum.MyGallery:
+          return UserPermissionService.UserClaimModel.IsAuthenticated;
+        case GalleryTypeEnum.AdminMode:
+          return UserPermissionService.UserPolicies.CanApprovePicture;
+      }
 
-        public PicturesGet(IFileManager fileManager)
-        {
-            _fileManager = fileManager;
-        }
+      return false;
+    }).Select(s => new SelectOptionDto<byte>()
+    {
+      Text = s.GetDisplayName(),
+      Value = (byte)s,
+    }).ToList();
+  }
 
-        /// <summary>
-        /// Gets all Pictures that the logged in user has access to.
-        /// </summary>
-        /// <param name="activeOnly">active only items</param>
-        public async Task<IEnumerable<PictureDto>> GetPictures(bool activeOnly)
-        {
-            return await (from x in db.PicturesByFilter(UserPermissionService, activeOnly)
-                          select new PictureDto()
-                          {
-                              IsApproved = x.ApprovedDateTime.HasValue,
-                              Description = x.Description,
-                              PictureId = x.PictureId,
-                              IsActive = !x.AuditFields.InactiveDateTime.HasValue,
-                          }).ToListAsync();
-        }
-
-        /// <summary>
-        /// Gets all pictures for gallery
-        /// </summary>
-        /// <param name="activeOnly">active only items</param>
-        public async Task<IEnumerable<PictureDto>> GetPicturesForBrowsing()
-        {
-            return await (from x in db.PicturesByFilter(UserPermissionService, true)
-                          where x.ApprovedDateTime.HasValue
-                          select new PictureDto()
-                          {
-                              IsApproved = x.ApprovedDateTime.HasValue,
-                              Description = x.Description,
-                              PictureId = x.PictureId,
-                              IsActive = !x.AuditFields.InactiveDateTime.HasValue,
-                          }).ToListAsync();
-        }
-
-        /// <summary>
-        /// Gets random pictures
-        /// </summary>
-        public async Task<IEnumerable<PictureDto>> GetRandomPictures()
-        {
-            return await (from x in db.PicturesByFilter(UserPermissionService, true)
-                          where x.ApprovedDateTime.HasValue
-                          select new PictureDto()
-                          {
-                              IsApproved = x.ApprovedDateTime.HasValue,
-                              Description = x.Description,
-                              PictureId = x.PictureId,
-                              IsActive = !x.AuditFields.InactiveDateTime.HasValue,
-                          }).OrderBy(o => Guid.NewGuid()).Take(5).ToListAsync();
-        }
-
-        /// <summary>
-        /// Gets my Pictures
-        /// </summary>
-        public async Task<IEnumerable<PictureDto>> GetMyPictures()
-        {
-            return await (from x in db.PicturesByFilter(UserPermissionService, false)
-                          where x.OwnerUserId == UserPermissionService.UserClaimModel.UserId
-                          select new PictureDto()
-                          {
-                              IsApproved = x.ApprovedDateTime.HasValue,
-                              Description = x.Description,
-                              PictureId = x.PictureId,
-                              IsActive = !x.AuditFields.InactiveDateTime.HasValue,
-                          }).ToListAsync();
-        }
-
-        /// <summary>
-        /// Gets gallery types the user has access to
-        /// </summary>
-        public List<SelectOptionDto<byte>> GetGalleryTypes()
-        {
-            return GalleryTypeEnum.AdminMode.GetList().Where(w =>
+  /// <summary>
+  /// Gets a Picture
+  /// </summary>
+  /// <param name="id">id to get</param>
+  /// <returns>dto</returns>
+  public Task<PictureDto?> GetPicture(int id)
+  {
+    return (from x in db.RoleRestrictedPictures(UserPermissionService)
+            where x.PictureId == id
+            select new PictureDto()
             {
-                switch (w)
-                {
-                    case GalleryTypeEnum.Public:
-                        return true;
-                    case GalleryTypeEnum.MyGallery:
-                        return UserPermissionService.UserClaimModel.IsAuthenticated;
-                    case GalleryTypeEnum.AdminMode:
-                        return UserPermissionService.UserPolicies.CanApprovePicture;
-                }
+              IsApproved = x.ApprovedDateTime.HasValue,
+              Description = x.Description,
+              PictureId = x.PictureId,
+              IsActive = !x.AuditFields.InactiveDateTime.HasValue,
+            }).SingleOrDefaultAsync();
+  }
 
-                return false;
-            }).Select(s => new SelectOptionDto<byte>()
-            {
-                Text = s.GetDisplayName(),
-                Value = (byte)s,
-            }).ToList();
-        }
+  /// <summary>
+  /// Gets a Picture
+  /// </summary>
+  /// <param name="id">id to get</param>
+  /// <returns>image file stream</returns>
+  public async Task<FileDownload> GetPictureForViewing(int id)
+  {
+    var response = new FileDownload();
+    var picture = await db.RoleRestrictedPictures(UserPermissionService).FirstOrDefaultAsync(w => w.PictureId == id);
+    if (picture == null)
+      return response;
 
-        /// <summary>
-        /// Gets a Picture
-        /// </summary>
-        /// <param name="id">id to get</param>
-        /// <returns>dto</returns>
-        public async Task<PictureDto> GetPicture(int id)
-        {
-            return await (from x in db.RoleRestrictedPictures(UserPermissionService)
-                          where x.PictureId == id
-                          select new PictureDto()
-                          {
-                              IsApproved = x.ApprovedDateTime.HasValue,
-                              Description = x.Description,
-                              PictureId = x.PictureId,
-                              IsActive = !x.AuditFields.InactiveDateTime.HasValue,
-                          }).SingleOrDefaultAsync();
-        }
+    var filePathInfo = new FilePathInfo()
+    {
+      Path = PathManager.GetUserGalleryPath(picture.OwnerUserId),
+      FileName = picture.DocumentObj.DocumentFilename,
+    };
 
-        /// <summary>
-        /// Gets a Picture
-        /// </summary>
-        /// <param name="id">id to get</param>
-        /// <returns>image file stream</returns>
-        public async Task<FileDownload> GetPictureForViewing(int id)
-        {
-            var response = new FileDownload();
-            var picture = await db.RoleRestrictedPictures(UserPermissionService).FirstOrDefaultAsync(w => w.PictureId == id);
-            if (picture == null)
-                return response;
-
-            var filePathInfo = new FilePathInfo()
-            {
-                Path = PathManager.GetUserGalleryPath(picture.OwnerUserId),
-                FileName = picture.DocumentObj.DocumentFilename,
-            };
-
-            using (var ms = new MemoryStream())
-            using (var stream = _fileManager.OpenFile(filePathInfo, true))
-            {
-                await stream.CopyToAsync(ms);
-                response.MimeType = picture.DocumentObj.MimeType;
-                ms.Position = 0;
-                response.FileBytes = ms.ToArray();
-            }
-            
-            return response;
-        }
+    using (var ms = new MemoryStream())
+    using (var stream = _fileManager.OpenFile(filePathInfo, true))
+    {
+      await stream.CopyToAsync(ms);
+      response.MimeType = picture.DocumentObj.MimeType;
+      ms.Position = 0;
+      response.FileBytes = ms.ToArray();
     }
+
+    return response;
+  }
 }
