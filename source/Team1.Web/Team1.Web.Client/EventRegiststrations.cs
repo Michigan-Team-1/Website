4d5ff61d796f41bbf24b7bbaf3b41e36bf38965e
Jeremy Smith <jsmith@mca.net>
2026-04-28 22:03:00 -0400

    Added animations to cards and fixed .Net 10 blazor updating.

---
+++ source/Team1.Web/Team1.Web.Client/EventRegiststrations.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Team1.Web.Client;
+
+[EventHandler("ontransitionend", typeof(EventArgs), enableStopPropagation: true, enablePreventDefault: false)]
+[EventHandler("onanimationend", typeof(EventArgs), enableStopPropagation: true, enablePreventDefault: false)]
+public static class EventHandlers
+{
+}
