1cbbf0cdeca353c50324fe40ab3519a67be2e6e7
Jace Williams <J.williams@glrealitylabs.com>
2026-06-16 10:38:13 -0400

    Started implementation of dynamic pdf generation to replace old static pdf

---
+++ source/Team1.Infrastructure/Services/MembershipPdfService.cs
@@ -0,0 +1,28 @@
+﻿using QuestPDF.Fluent;
+using QuestPDF.Helpers;
+using QuestPDF.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Team1.Infrastructure.Services;
+
+public class MembershipPdfService : BaseService
+{
+    public byte[] GenerateApplication()
+    {
+        QuestPDF.Settings.License = LicenseType.Community;
+
+        return Document.Create(container =>
+        {
+            container.Page(page =>
+            {
+                page.Size(PageSizes.Letter);
+                page.Margin(1, Unit.Inch);
+                page.DefaultTextStyle(x => x.FontSize(12));
+
+                page.Content().Text("Test");
+            });
+        }).GeneratePdf();
+    }
+}
---
+++ source/Team1.Web/Team1.Web/Controllers/DownloadsController.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using Team1.Infrastructure.Services;
+
+namespace Team1.Web.Controllers;
+
+[Produces("application/json")]
+[Route("api/Downloads")]
+public class DownloadsController : BaseController
+{
+    [HttpGet("membership-application")]
+    public IActionResult MembershipApplication()
+    {
+        var service = GetService<MembershipPdfService>();
+        var pdfBytes = service.GenerateApplication();
+
+        return File(pdfBytes, "application/pdf", "Michigan-Team1-Membership-Application.pdf");
+    }
+}
